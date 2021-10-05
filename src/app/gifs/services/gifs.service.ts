cb22f1833ac6bfede8f644434fbd2922fb3c6ce4
Juan Garcia <juan.off.182@gmail.com>
2021-10-05 11:30:57 -0500

    added persistence when refresh the previous search, both history and images

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,8 +17,14 @@ export class GifsService {
     return [...this._historial];    
   }
 
+
   constructor (private http: HttpClient) {
 
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    this.resultados = JSON.parse( localStorage.getItem('resultado')! ) || [];
+    // if( localStorage.getItem('historial') ) {
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
+    // }
   }
 
   buscarGifs( query: string='') {    
@@ -28,12 +34,16 @@ export class GifsService {
     if (!this._historial.includes( query ) ) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ) );
+
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=HJipK4MYM0jzSAdxz1sKdLjSGRSkfqtc&q=${ query }&limit=10`)
         .subscribe( resp  => {
           console.log( resp.data )
-          this.resultados = resp.data;
+          this.resultados = resp.data;          
+          localStorage.setItem('resultado', JSON.stringify( this.resultados ) );
         });
 
 
